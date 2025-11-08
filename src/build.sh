5c1c533613961f2f1224dcc389661fa3c5950715
Adib Saikali <asaikali@gmail.com>
2025-11-08 06:14:45 -0500

    feat: vendor in the external secrets operator

--- src/build.sh
+++ src/build.sh
@@ -8,15 +8,22 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 PLATFORM_DIR=${REPO_ROOT}/platform
 
-FLUX_YAML="${PLATFORM_DIR}/flux-system/gotk-components.yaml"
-CERT_MANER="${PLATFORM_DIR}/cert-manager/cert-manager.yaml"
+FLUX="${PLATFORM_DIR}/flux-system/gotk-components.yaml"
+CERT_MANAGER="${PLATFORM_DIR}/cert-manager/cert-manager.yaml"
+ESO="${PLATFORM_DIR}/external-secrets/external-secrets.yaml"
 
 # Build the bootstrap manifest
 echo "Running kustomize build..."
-kustomize build components/flux > ${FLUX_YAML}
-kustomize build components/cert-manager > ${CERT_MANER}
+kustomize build components/flux > ${FLUX}
+kustomize build components/cert-manager > ${CERT_MANAGER}
+
+helm template external-secrets \
+    components/external-secrets/upstream/chart \
+    -n external-secrets \
+    --create-namespace > ${ESO}
 
 
 echo "Generated files:"
-echo "  - ${FLUX_YAML}"
-echo "  - ${CERT_MANER}"
+echo "  - ${FLUX}"
+echo "  - ${CERT_MANAGER}"
+echo "  - ${ESO}"
