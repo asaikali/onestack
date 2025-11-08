b5e4eb2422aeb088ab3937428d7396cddbd4f7b8
Adib Saikali <asaikali@gmail.com>
2025-11-08 07:20:41 -0500

    feat: vendor in enjoy gateway

--- src/build.sh
+++ src/build.sh
@@ -11,6 +11,8 @@ PLATFORM_DIR=${REPO_ROOT}/platform
 FLUX="${PLATFORM_DIR}/flux-system/gotk-components.yaml"
 CERT_MANAGER="${PLATFORM_DIR}/cert-manager/cert-manager.yaml"
 ESO="${PLATFORM_DIR}/external-secrets/external-secrets.yaml"
+ENVOY_GATEWAY="${PLATFORM_DIR}/envoy-gateway/envoy-gateway.yaml"
+
 
 # Build the bootstrap manifest
 echo "Running kustomize build..."
@@ -20,12 +22,22 @@ kustomize build components/cert-manager > ${CERT_MANAGER}
 helm template external-secrets \
     components/external-secrets/upstream/chart \
     -n external-secrets \
+    --include-crds \
     --create-namespace >  components/external-secrets/upstream/rendered-external-secrets.yaml
 
 kustomize build components/external-secrets > ${ESO}
+
+helm template envoy-gateway \
+    components/envoy-gateway/upstream/chart/gateway-helm \
+    -n envoy-gateway-systems \
+    --include-crds \
+    --create-namespace >  components/envoy-gateway/upstream/rendered-envoy-gateway.yaml
+
+kustomize build components/envoy-gateway > ${ENVOY_GATEWAY}
 
 
 echo "Generated files:"
 echo "  - ${FLUX}"
 echo "  - ${CERT_MANAGER}"
 echo "  - ${ESO}"
+echo "  - ${ENVOY_GATEWAY}"
