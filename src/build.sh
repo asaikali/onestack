afbfcb5a4c5a9180b7cb864e812e4c463e4adf16
Adib Saikali <asaikali@gmail.com>
2025-11-08 01:26:40 -0500

    feature: Generate flux manifests

---
+++ src/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+vendir sync
+
+# Define the variable REPO_ROOT (Absolute path to the repository root)
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+PLATFORM_DIR=${REPO_ROOT}/platform
+
+FLUX_YAML="${PLATFORM_DIR}/bootstrap/flux.yaml"
+
+# Build the bootstrap manifest
+echo "Running kustomize build..."
+kustomize build components/flux > ${FLUX_YAML}
+
+
+echo "Generated files:"
+echo "  - ${FLUX_YAML}"
