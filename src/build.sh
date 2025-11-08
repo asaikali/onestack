75af1f17e2993439c17b84759907a3d707ecbb47
Adib Saikali <asaikali@gmail.com>
2025-11-08 04:04:37 -0500

    feat: vendor in the cert-manager helm chart

--- src/build.sh
+++ src/build.sh
@@ -9,11 +9,14 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 PLATFORM_DIR=${REPO_ROOT}/platform
 
 FLUX_YAML="${PLATFORM_DIR}/flux-system/gotk-components.yaml"
+CERT_MANER="${PLATFORM_DIR}/cert-manager/cert-manager.yaml"
 
 # Build the bootstrap manifest
 echo "Running kustomize build..."
 kustomize build components/flux > ${FLUX_YAML}
+kustomize build components/cert-manager > ${CERT_MANER}
 
 
 echo "Generated files:"
 echo "  - ${FLUX_YAML}"
+echo "  - ${CERT_MANER}"
